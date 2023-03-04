7f6940c6b40e266eee1f813319ea15171d6c60e8
ItzNotAsh <chawlaashneet@gmail.com>
2023-03-04 21:55:49 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+const { TOKEN } = require('./config.json');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}🪄🍭🗽!`);
   });
+
+client.on('messageCreate' , (msg) => {
+    console.log(`${msg.content}`);
+});
+
+  
+  client.login(TOKEN);
