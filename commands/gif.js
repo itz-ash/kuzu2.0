e4ec2f7161e0e13368e3b9ddd4bfaca9f3d79d6b
ItzNotAsh <chawlaashneet@gmail.com>
2023-03-05 11:58:18 +0530

    args

--- commands/gif.js
+++ commands/gif.js
@@ -5,12 +5,19 @@ module.exports = function (msg, args) {
         return;
     }
     
+        // Join the arguments into a single string
+        args_str = args.join(' ');
+
+        // Replace all consecutive spaces with one space
+        args_str = args_str.replace(/ +/g, ' ');
+    
+        // Split the string back into an array of arguments
+        args = args_str.split(' ');
+
     len = args.length;
     content = '';
-    tmpcontent = '';
     for(i = 0; i < len;i++){
-        tmpcontent = `${i + 1}  ${args[i]} \n`;
-        content = content + tmpcontent;
+        content += `${i + 1}  ${args[i]}\n`;
     }
     msg.channel.send(content);
 }
