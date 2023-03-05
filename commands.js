4e61fb98c47f4118f713d315192c95d3ff1971d1
ItzNotAsh <chawlaashneet@gmail.com>
2023-03-05 12:41:39 +0530

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,14 +1,29 @@
+const fs = require('fs');
+const path = require('path');
 
+const commands = {};
 
-const gif = require('./commands/gif.js');
-const kuzu = require('./commands/kuzu.js');
-const commands = { kuzu, gif };
+// Get all files in the commands directory
+const commandFiles = fs.readdirSync(path.join(__dirname, 'commands'));
+
+// Loop through each file and add it to the commands object
+for (const file of commandFiles) {
+  const commandName = file.split('.')[0]; // remove file extension
+  const command = require(`./commands/${file}`);
+
+  commands[commandName] = command;
+}
+
 module.exports = async function (msg){
-
     let tokens = msg.content.split(" ");
     let command = tokens.shift();
+
     if (command.charAt(0) === '!'){
         command = command.substring(1);
-        commands[command](msg, tokens)
+
+        if (command in commands) {
+          commands[command](msg, tokens);
+        }
     }
 }
+
